dfb7181ae3244b8d212321341ed52427ed4d74dd
Rosa <rosarenee@att.net>
2021-07-09 21:26:14 -0500

    commit tree bbfd84b589711a3064b0f5a207ece734379c75d2

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+function createEmployeeRecord([string1= "Gray", string2 ="Worm", string3= "Security", num =1]){
+
+ 
+    let firstName = 'Gray'
+    let familyName = string2
+    // title:
+    // payPerHour:
+    // timeInEvents:
+    // timeOutEvents:
+    
+
+}
